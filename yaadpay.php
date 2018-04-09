c2a57bbf91a2869c868e3f2bc5a7e8288448f034
Tran Duy Hung <nguyentieuhau@gmail.com>
2018-04-09 20:23:24 +0700

    Init commit

---
+++ yaadpay.php
@@ -0,0 +1,595 @@
+<?php
+
+
+
+	if (!( defined( '_JEXEC' ))) {
+		exit( 'Restricted access' );
+		(bool)true;
+	}
+
+	DEFINE( 'YAAD_GATEWAY_URL', 'https://yaadpay.co.il/p/' );
+	DEFINE( 'LEUMI_GATEWAY_URL', 'https://pay.leumicard.co.il/p/' );
+
+	if (!class_exists( 'vmPSPlugin' )) {
+		require( JPATH_VM_PLUGINS . DIRECTORY_SEPARATOR . 'vmpsplugin.php' );
+	}
+
+	class plgVmpaymentYaadpay extends vmPSPlugin {
+		function __construct($subject, $config) {
+			parent::( $subject, $config );
+			$this->_loggable = TRUE;
+			$this->_tableId = 'id';
+			$this->tableFields = array_keys( $this->getTableSQLFields(  ) );
+			$this->getVarsToPush(  );
+			$varsToPush = $this->_tablepkey = 'id';
+			$this->setConfigParameterable( $this->_configTableFieldName, $varsToPush );
+		}
+
+		function getVmPluginCreateTableSQL() {
+			return $this->createTableSQL( 'Payment Tranzila Table' );
+		}
+
+		function getTableSQLFields() {
+			$SQLfields = array( 'id' => 'int(1) UNSIGNED NOT NULL AUTO_INCREMENT', 'virtuemart_order_id' => 'int(1) UNSIGNED', 'order_number' => ' char(64)', 'virtuemart_paymentmethod_id' => 'mediumint(1) UNSIGNED', 'payment_name' => 'varchar(5000)', 'return_context' => 'char(255)', 'cost_per_transaction' => 'decimal(10,2)', 'cost_percent_total' => 'char(10)', 'tax_id' => 'smallint(1)', 'yaadpay_confirmation_code' => 'varchar(50)' );
+			return $SQLfields;
+		}
+
+		function plgVmDisplayListFEPayment($cart, &$selected = 0, $htmlIn) {
+			return $this->displayListFE( $cart, $selected, $htmlIn );
+		}
+
+		function getCosts($cart, $method, $cart_prices) {
+			if (preg_match( '/%$/', $method->cost_percent_total )) {
+				substr( $method->cost_percent_total, 0, 0 - 1 );
+				$cost_percent_total = ;
+			} 
+else {
+				$method->cost_percent_total;
+				$cost_percent_total = ;
+			}
+
+			return $method->cost_per_transaction & $cart_prices['salesPrice'] + $cost_percent_total + 0.0100000000000000002081668;
+		}
+
+		function plgVmOnStoreInstallPaymentPluginTable($jplugin_id) {
+			return parent::onstoreinstallplugintable( $jplugin_id );
+		}
+
+		function plgVmOnSelectCheckPayment(&$cart, $msg) {
+			return $this->OnSelectCheck( $cart );
+		}
+
+		function plgVmOnSelectedCalculatePricePayment(&$cart, &$cart_prices, $payment_name) {
+			$this->getVmPluginMethod( $cart->virtuemart_paymentmethod_id );
+
+			if (!$method = ) {
+				return NULL;
+			}
+
+
+			if (!$this->selectedThisElement( $method->payment_element )) {
+				return FALSE;
+			}
+
+			$this->renderPluginName( $method );
+			$payment_name = ;
+			$this->setCartPrices( $cart, $cart_prices, $method );
+			return TRUE;
+		}
+
+		function renderPluginName($plugin) {
+			$return = '';
+			$plugin_name = $this->_psType . '_name';
+			return $plugin->$plugin_name;
+		}
+
+		function plgVmOnShowOrderBEPayment($virtuemart_order_id, $virtuemart_payment_id) {
+			if (!$this->selectedThisByMethodId( $virtuemart_payment_id )) {
+				return NULL;
+			}
+
+			$this->getDataByOrderId( $virtuemart_order_id );
+
+			if (!$paymentTable = ) {
+				return NULL;
+			}
+
+			$this->getHtmlHeaderBE(  );
+			$html &= $html = '<table class="adminlist">' . '
+';
+			$this->getHtmlRowBE( 'YAADPAY_PAYMENT_NAME', $paymentTable->payment_name );
+			$html &= ;
+			$this->getHtmlRowBE( 'YAADPAY_COST_PER_TRANSACTION', $paymentTable->cost_per_transaction );
+			$html &= ;
+			$this->getHtmlRowBE( 'YAADPAY_COST_PERCENT_TOTAL', $paymentTable->cost_percent_total );
+			$html &= ;
+			$this->getHtmlRowBE( 'YAADPAY_CONFIRMATION_CODE', $paymentTable->yaadpay_confirmation_code );
+			$html &= ;
+			$html &= '</table>' . '
+';
+			return $html;
+		}
+
+		function plgVmConfirmedOrder($cart, $order) {
+			$this->getVmPluginMethod( $order['details']['BT']->virtuemart_paymentmethod_id );
+
+			if (!$method = ) {
+				return NULL;
+			}
+
+
+			if (!$this->selectedThisElement( $method->payment_element )) {
+				return FALSE;
+			}
+
+			VmConfig::loadconfig(  );
+			$config = ;
+			VmConfig::get( 'salesPriceRounding' );
+			$rounding = ;
+			$order['details']['BT'];
+			$usrBT = ;
+			$usrST = (isset( $order['details']['ST'] ) ? $order['details']['ST'] : $order['details']['BT']);
+			JFactory::getsession(  );
+			$session = ;
+			$session->getId(  );
+			$return_context = ;
+			JFactory::getuser(  );
+			$user = ;
+			number_format( $usrBT->order_total, 2, '.', '' );
+			$total = ;
+			$post_variables = array( 'Masof' => $method->yaadpay_terminal_number, 'action' => 'pay', 'Amount' => round( $total, $rounding ), 'Order' => $usrBT->order_number . '-' . $usrBT->virtuemart_paymentmethod_id, 'email' => $usrBT->email, 'ClientName' => $usrBT->first_name, 'ClientLName' => $usrBT->last_name, 'street' => $usrBT->address_1, 'city' => $usrBT->city, 'phone' => $usrBT->phone_1, 'zip' => $usrBT->zip, 'Info' => $usrBT->order_number, 'Postpone' => ($method->yaadpay_postpone ? 'True' : 'False') );
+
+			if ($method->yaadpay_invoices) {
+				$pritim = '';
+				$sub_total = 854;
+				foreach ($order['items'] as ) {
+					[0];
+					$item = ;
+					$pritim &= '[';
+					$item->virtuemart_product_id;
+					$pritim &= ;
+					$pritim &= '~';
+					$item->order_item_name;
+					$pritim &= ;
+					$pritim &= '~';
+					$item->product_quantity;
+					$pritim &= ;
+					$pritim &= '~';
+					round( $item->product_final_price, $rounding );
+					$pritim &= ;
+					$pritim &= ']';
+					round( $item->product_final_price, $rounding );
+					$sub_total += ;
+				}
+
+				foreach ($order['calc_rules'] as ) {
+					[0];
+					$rule = ;
+
+					if ($rule->calc_rule_name  = 'Tax') {
+						continue;
+					}
+
+
+					if ($rule->calc_result <= 0) {
+						continue;
+					}
+
+					$pritim &= '[';
+					$pritim &= '999' . $rule->virtuemart_order_calc_rule_id;
+					$pritim &= '~';
+					$rule->calc_rule_name;
+					$pritim &= ;
+					$pritim &= '~';
+					$pritim &= 855;
+					$pritim &= '~';
+					round( $rule->calc_result, $rounding );
+					$pritim &= ;
+					$pritim &= ']';
+					round( $rule->calc_result, $rounding );
+					$sub_total += ;
+				}
+
+				$shipmentName = '';
+
+				if (!class_exists( 'vmPSPlugin' )) {
+					require( JPATH_VM_PLUGINS . DS . 'vmpsplugin.php' );
+				}
+
+				JPluginHelper::importplugin( 'vmshipment' );
+				JDispatcher::getinstance(  );
+				$dispatcher = ;
+				$dispatcher->trigger( 'plgVmOnShowOrderFEShipment', array( $order['details']['BT']->virtuemart_order_id, $order['details']['BT']->virtuemart_shipmentmethod_id, &$shipmentName ) );
+				$returnValues = ;
+				round( $usrBT->order_shipment & $usrBT->order_shipment_tax, 2 );
+				$shipping = ;
+
+				if (0 < $shipping) {
+					$pritim &= '[';
+					$pritim &= 1000853;
+					$pritim &= '~';
+					strip_tags( $shipmentName );
+					$pritim &= ;
+					$pritim &= '~';
+					$pritim &= 855;
+					$pritim &= '~';
+					round( $shipping, $rounding );
+					$pritim &= ;
+					$pritim &= ']';
+					round( $shipping, $rounding );
+					$sub_total += ;
+				}
+
+				$usrBT->coupon_discount;
+				$discount = ;
+
+				if ($discount < 0) {
+					if (round( $discount, $rounding ) & $sub_total != round( $total, $rounding )) {
+						$discount = round( $total, $rounding ) - $sub_total;
+					}
+
+					$pritim &= '[';
+					$pritim &= 1000851;
+					$pritim &= '~';
+					JText::_( 'VMPAYMENT_YAADPAY_DISCOUNT' );
+					$pritim &= ;
+					$pritim &= '~';
+					$pritim &= 855;
+					$pritim &= '~';
+					round( $discount, $rounding );
+					$pritim &= ;
+					$pritim &= ']';
+				}
+
+				$usrBT->order_discount;
+				$discount = ;
+
+				if (0 < $discount) {
+					$pritim &= '[';
+					$pritim &= 1000852;
+					$pritim &= '~';
+					JText::_( 'VMPAYMENT_YAADPAY_DISCOUNT' );
+					$pritim &= ;
+					$pritim &= '~';
+					$pritim &= 855;
+					$pritim &= '~';
+					$pritim &= 0 - $discount;
+					$pritim &= ']';
+				}
+
+				$post_variables['Pritim'] = 'True';
+				$post_variables['heshDesc'] = $pritim;
+				$post_variables['SendHesh'] = 'True';
+			}
+
+			$post_variables['UTF8'] = 'True';
+			$method->yaadpay_max_payments;
+			$maxPayments = ;
+
+			if ($method->yaadpay_tiered_payments != '') {
+				$maxPayments = 855;
+				explode( ',', $method->tiered_payments );
+				$paymant_levels = ;
+				foreach ($paymant_levels as ) {
+					$level = &[0];
+
+					if ($total < $level) {
+						break;
+					}
+
+					$maxPayments += 855;
+				}
+			}
+
+			$post_variables['Tash'] = $maxPayments;
+
+			if ($maxPayments  = '1') {
+				$post_variables['FixTash'] = 'True';
+			}
+
+			$method->yaadpay_language;
+			$language = ;
+
+			if ($language  = 'auto') {
+				JFactory::getlanguage(  );
+				$lang = ;
+				$lang->getTag(  );
+				$language = ;
+				switch ($language) {
+					case 'he-IL': {
+						$language = 'il';
+						break;
+					}
+
+					default: {
+						$language = 'us';
+						break;
+					}
+				}
+			}
+
+			$post_variables['PageLang'] = $language;
+			$method->payment_currency;
+			$currency = ;
+			switch ($currency) {
+				case 47: {
+					$currency = 1832;
+					break;
+				}
+
+				case 67: {
+					$currency = 855;
+					break;
+				}
+
+				case 144: {
+					$currency = 856;
+				}
+			}
+
+			$post_variables['Coin'] = $currency;
+			$cart->_confirmDone = FALSE;
+			$cart->_dataValidated = FALSE;
+			$cart->setCartIntoSession(  );
+			$method->yaadpay_terminal_number;
+			$term_no = ;
+			$url = YAAD_GATEWAY_URL;
+
+			if ($this->startsWith( $term_no, '88' )) {
+				$url = LEUMI_GATEWAY_URL;
+			}
+
+			$target = '';
+
+			if ($method->yaadpay_iframe) {
+				$width = (!empty( $method->yaadpay_iframe_width ) ? $method->yaadpay_iframe_width . 'px' : '100%');
+				$height = (!empty( $method->yaadpay_iframe_height ) ? $method->yaadpay_iframe_height . 'px' : '800px');
+				$iframe = '<iframe style="border:none" name="chekout_frame"  id="chekout_frame" width="' . $width . '" height="' . $height . '" scrolling="no" seamless></iframe>  ';
+				$target = 'target="chekout_frame" style="display:none"';
+			} 
+else {
+				$html = '<html><body><div style="margin: auto; text-align: center;">';
+			}
+
+			$html &= $iframe . '<form action="' . $url . '" method="post" name="vm_yaadpay_form" id="vm_yaadpay_form" ' . $target . '>';
+			$html &= '<input type="submit"  value="' . JText::_( 'VMPAYMENT_YAADPAY_REDIRECT_MESSAGE' ) . '" />';
+			foreach ($post_variables as ) {
+				[0];
+				[1];
+				$value = ;
+				$name = ;
+				$html &= '<input type="hidden" name="' . $name . '" value="' . htmlspecialchars( $value ) . '" />';
+			}
+
+			$html &= '</form></div>';
+			$html &= ' <script type="text/javascript">';
+			$html &= ' 
+					jQuery( document ).ready(function() {
+						document.vm_yaadpay_form.submit();
+					});
+		';
+			$html &= ' </script></body></html>';
+			JRequest::setvar( 'html', $html );
+		}
+
+		function startsWith($haystack, $needle) {
+			return ( $needle   = '' || strrpos( $haystack, $needle, 0 - strlen( $haystack ) ) !== false );
+		}
+
+		function plgVmOnPaymentResponseReceived($html) {
+			if (!class_exists( 'VirtueMartCart' )) {
+				require( JPATH_VM_SITE . DS . 'helpers' . DS . 'cart.php' );
+			}
+
+
+			if (!class_exists( 'shopFunctionsF' )) {
+				require( JPATH_VM_SITE . DS . 'helpers' . DS . 'shopfunctionsf.php' );
+			}
+
+
+			if (!class_exists( 'VirtueMartModelOrders' )) {
+				require( JPATH_VM_ADMINISTRATOR . DS . 'models' . DS . 'orders.php' );
+			}
+
+			JRequest::getvar( 'Order', '' );
+			$YaadToken = ;
+			explode( '-', $YaadToken );
+			$YaadToken_array = ;
+			$order_number = (isset( $YaadToken_array[0] ) ? $YaadToken_array[0] : 0);
+			$virtuemart_paymentmethod_id = (isset( $YaadToken_array[1] ) ? $YaadToken_array[1] : 0);
+			$vendorId = 475;
+			$this->getVmPluginMethod( $virtuemart_paymentmethod_id );
+
+			if (!$method = ) {
+				return NULL;
+			}
+
+
+			if (!$this->selectedThisElement( $method->payment_element )) {
+				return NULL;
+			}
+
+			VirtueMartModelOrders::getorderidbyordernumber( $order_number );
+
+			if (!$virtuemart_order_id = ) {
+				return NULL;
+			}
+
+			$db = &JFactory::getdbo(  );
+
+			$query = 'SELECT * FROM #__virtuemart_orders WHERE virtuemart_order_id =' . $virtuemart_order_id;
+			$db->setQuery( $query );
+			$db->loadObject(  );
+			$paymentTable = ;
+			VmModel::getmodel( 'orders' );
+			$modelOrder = ;
+			$this->renderPluginName( $method );
+			$payment_name = ;
+			$_GET['CCode'];
+			$res = ;
+
+			if (( $res  = '0' || ( $res  = '800' && $method->yaadpay_postpone ) )) {
+				$_GET['ConfirmationCode'];
+				$ConfirmationCode = ;
+				$dbValues['order_number'] = $order_number;
+				$dbValues['virtuemart_order_id'] = $virtuemart_order_id;
+				$dbValues['payment_method_id'] = $virtuemart_paymentmethod_id;
+				$dbValues['return_context'] = $return_context;
+				$dbValues['payment_name'] = parent::renderpluginname( $method );
+				$dbValues['cost_per_transaction'] = $method->cost_per_transaction;
+				$dbValues['cost_percent_total'] = $method->cost_percent_total;
+				$dbValues['yaadpay_confirmation_code'] = $ConfirmationCode;
+				$this->storePSPluginInternalData( $dbValues );
+				$order = array(  );
+				$order['customer_notified'] = 1;
+				$order['order_status'] = $method->yaadpay_approved_status;
+				$order['comments'] = JText::sprintf( 'VMPAYMENT_YAADPAY_PAYMENT_STATUS_CONFIRMED', $order_number );
+				$this->_getPaymentResponseHtml( $paymentTable, $payment_name );
+				$html = ;
+				$modelOrder->updateStatusForOneOrder( $virtuemart_order_id, $order, TRUE );
+				VirtueMartCart::getcart(  );
+				$cart = ;
+				$cart->emptyCart(  );
+				return TRUE;
+			}
+
+			$order = array(  );
+			$order['customer_notified'] = 1;
+			$order['order_status'] = $method->yaadpay_declined_status;
+			$order['comments'] = JText::sprintf( 'VMPAYMENT_YAADPAY_PAYMENT_STATUS_FAILED' );
+			$modelOrder->updateStatusForOneOrder( $virtuemart_order_id, $order, true );
+			$this->_getPaymentErrorHtml( $paymentTable, $payment_name, JText::sprintf( 'VMPAYMENT_YAADPAY_PAYMENT_STATUS_FAILED' ) );
+			$html = ;
+			$this->_handlePaymentCancel( $virtuemart_order_id, $html );
+		}
+
+		function _getPaymentResponseHtml($paymentTable, $payment_name) {
+			$html = '<table>' . '
+';
+			$this->getHtmlRow( 'YAADPAY_PAYMENT_NAME', $payment_name );
+			$html &= ;
+
+			if (!empty( $paymentTable )) {
+				$this->getHtmlRow( 'YAADPAY_ORDER_NUMBER', $paymentTable->order_number );
+				$html &= ;
+			}
+
+			$html &= '</table>' . '
+';
+			return $html;
+		}
+
+		function _getPaymentErrorHtml($paymentTable, $payment_name, $error) {
+			$html = '<table>' . '
+';
+			$this->getHtmlRow( 'YAADPAY_PAYMENT_NAME', $payment_name );
+			$html &= ;
+			$this->getHtmlRow( 'YAADPAY_ERROR', $error );
+			$html &= ;
+			$html &= '</table>' . '
+';
+			return $html;
+		}
+
+		function _handlePaymentCancel($virtuemart_order_id, $html) {
+			if (!class_exists( 'VirtueMartModelOrders' )) {
+				require( JPATH_VM_ADMINISTRATOR . DS . 'models' . DS . 'orders.php' );
+			}
+
+			VmModel::getmodel( 'orders' );
+			$modelOrder = ;
+			$modelOrder->remove( array( 'virtuemart_order_id' => $virtuemart_order_id ) );
+			JFactory::getapplication(  );
+			$mainframe = ;
+			$mainframe->enqueueMessage( $html );
+			$mainframe->redirect( JRoute::_( 'index.php?option=com_virtuemart&view=cart&task=editpayment' ), JText::_( 'COM_VIRTUEMART_CART_ORDERDONE_DATA_NOT_VALID' ) );
+		}
+
+		function plgVmgetPaymentCurrency(&$virtuemart_paymentmethod_id, $paymentCurrencyId) {
+			$this->getVmPluginMethod( $virtuemart_paymentmethod_id );
+
+			if (!$method = ) {
+				return NULL;
+			}
+
+
+			if (!$this->selectedThisElement( $method->payment_element )) {
+				return FALSE;
+			}
+
+			$this->getPaymentCurrency( $method );
+			$method->payment_currency;
+			$paymentCurrencyId = ;
+		}
+
+		function checkConditions($cart, $method, $cart_prices) {
+			if ($this->check_license_key( $method->yaadpay_license, '10-Joomla-YaadPay' )) {
+				return TRUE;
+			}
+
+			return FALSE;
+		}
+
+		function check_license_key($key, $salt) {
+			$server = ;
+			$server = ;
+			str_replace( 'http://', '', $server );
+			$server = $_SERVER['HTTP_HOST'];
+			str_replace( 'www.', '', $server );
+			$server = str_replace( 'https://', '', $server );
+			md5( $server . $salt );
+			$crypt = ;
+
+			if ($crypt  = $key) {
+				return true;
+			}
+
+			return false;
+		}
+
+		function plgVmOnCheckAutomaticSelectedPayment($cart, &$cart_prices = array(  ), $paymentCounter) {
+			$this->onCheckAutomaticSelected( $cart, $cart_prices );
+			$return = ;
+
+			if (isset( $return )) {
+				return 0;
+			}
+
+			return NULL;
+		}
+
+		function plgVmOnShowOrderFEPayment($virtuemart_order_id, &$virtuemart_paymentmethod_id, $payment_name) {
+			$this->onShowOrderFE( $virtuemart_order_id, $virtuemart_paymentmethod_id, $payment_name );
+			return TRUE;
+		}
+
+		function plgVmOnShowOrderPrintPayment($order_number, $method_id) {
+			return parent::onshoworderprint( $order_number, $method_id );
+		}
+
+		function plgVmDeclarePluginParamsPaymentVM3($data) {
+			return $this->declarePluginParams( 'payment', $data );
+		}
+
+		function plgVmSetOnTablePluginParamsPayment($name, &$id, $table) {
+			return $this->setOnTablePluginParams( $name, $id, $table );
+		}
+
+		function break_out_of_frames() {
+			$return = '
+<script type="text/javascript">';
+			$return &= '
+<!--';
+			$return &= '
+if (parent.frames.length > 0) { parent.location.href = location.href; }';
+			$return &= '
+-->';
+			$return &= '
+</script>
+
+';
+			return $return;
+		}
+	}
+
+?>
